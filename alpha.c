95811a2e7e4f57265769ba8cc1ac3ad8004a2784
gokulavani <dhilipkumarvv@gmail.com>
2017-06-06 14:38:35 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter the character\n");
+scanf("%c",&ch);
+if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+printf("The character is alphabet");
+else
+printf("The character is not alphabet");
+}
